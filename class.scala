ba68ea0c644aef2a32163db350a6c60927715c19
Daigo Hamasaki <daigo.hamasaki.16@gmail.com>
2018-07-15 22:04:08 +0900

    Classes

---
+++ class.scala
@@ -0,0 +1,40 @@
+class Point(var x: Int, var y: Int) {
+
+  def move(dx: Int, dy: Int): Unit = {
+    x = x + dx
+    y = y + dy
+  }
+
+  override def toString: String =
+    s"($x, $y)"
+}
+
+class Point2 {
+  private var _x = 0
+  private var _y = 0
+  private val bound = 100
+
+  def x = _x
+  def x_= (newValue: Int): Unit = {
+    if (newValue < bound) _x = newValue else printWarning
+  }
+
+  def y = _y
+  def y_= (newValue: Int): Unit = {
+    if (newValue < bound) _y = newValue else printWarning
+  }
+
+  private def printWarning = println("WARNING: Out of bounds")
+}
+
+object Point extends App {
+  val point1 = new Point(2, 3)
+  point1.x
+  println(point1)
+}
+
+object Point2 extends App {
+  val point1 = new Point2
+  point1.x = 99
+  point1.y = 101 // prints the warning
+}
